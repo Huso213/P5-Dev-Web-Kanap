df72ba1b9748dba7b395635d3d15018d9bf909da
Huso213 <huso_95400@hotmail.fr>
2023-08-12 01:29:34 +0200

    debut panier

--- front/js/confirmation.js
+++
@@ -1,6 +0,0 @@
-// affiche la confirmation et numero de la commande
-const orderId = JSON.parse(localStorage.getItem("order")) || [];
-let informations = document.querySelector("#orderId");
-if(informations != null) {
-    informations.innerHTMl += '${orderId.orderId}';
-}
