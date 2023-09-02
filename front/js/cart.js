cfc64053e14c1d39cd463022f676d0cc208c3089
Huso213 <huso_95400@hotmail.fr>
2023-09-03 00:18:32 +0200

    panier fonction sans regex

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,20 +1,22 @@
 //Page Panier
+
 cart = JSON.parse(localStorage.getItem("products")) || [];
 
-//si le panier contient un produit
+// Si le panier contient au moins 1 produit
 if (cart.length > 0) {
-    for(product of cart) {
+    for (product of cart) {
         displayCart(product);
     }
-    productIncart();
-    displayCart();
+    productsInCart();
+    displayTotalCart();
 }
 
 // Affichage des produits présents dans le panier
 function displayCart(product) {
   const indexProduct = cart.indexOf(product);
   const productList = document.getElementById("cart__items");
-  if (productList != null) {
+  if( productList != null) {
+
     productList.innerHTML += `
         <article class="cart__item" data-id="${product.id}">
         <div class="cart__item__img">
@@ -43,5 +45,68 @@ function displayCart(product) {
     </article>
     `;
   }
+}
+
+//Afficher le nombre d'article dans panier
+function productsInCart() {
+  let productNumbers = document.getElementById("totalQuantity");
+
+  if (cart.length > 0) {
+      let productInCart = 0;
+      for (product of cart) {
+          productInCart += product.quantity;
+      }
+      if (productNumbers != null) {
+          productNumbers.innerHTML += `${productInCart}`;
+      }
+  }
+}
+
+  //Calcul du montant total du panier
+  function updateTotalCost () {
+    let totalCart = 0;
+    cart.forEach((product) => {
+        totalCart = totalCart + (product.quantity * product.price);
+    });
+    return totalCart;
+}
+
+  //Affichage du montant total du panier
+  function displayTotalCart() {
+    const totalContent = document.getElementById("totalPrice");
+   if (totalContent != null) {
+       totalContent.innerHTML += updateTotalCost();
+   }
+}
+
+//gestion des quantites produits
+const inputList = document.querySelectorAll(".itemQuantity");
+for (input of inputList) {
+    let ind = input.getAttribute("data-index");
+    input.addEventListener("click", (e) => {
+        const newValue = e.target.value;
+        for (products of cart) {
+            let index = cart.indexOf(products);
+            let qty = cart[index].quantity;
+            if(index == ind && newValue > qty) {
+                cart[index].quantity++;
+                localStorage.setItem("products", JSON.stringify(cart));
+                location.reload();
+            }else if (index == ind && newValue < qty) {
+                cart[index].quantity--;
+                localStorage.setItem("products", JSON.stringify(cart));
+                location.reload();
+            } else {
+                newValue == qty;
+            }
+        }
+    });
+}
+//suprimer le produit
+function remove(index) {
+  cart.splice(index, 1);
+  localStorage.setItem("products", JSON.stringify(cart));
+  location.reload();
+  displayCart();
 }
 
